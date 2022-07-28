861265a0370688d9fc9178aa3f3fb1fa4a777e48
Santiago Barrera <santiago.barrerae@gmail.com>
2022-07-27 23:06:22 -0500

    timer initialized

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.scss']
+  styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnInit {
+  timer: Date = new Date();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.timer.setHours(0, 25, 0);
   }
 
+  ngOnInit(): void {}
 }
