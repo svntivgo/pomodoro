a6044c8c7e7c5ba25d414679bd1592ee23ecad7c
Santiago Barrera <santiago.barrerae@gmail.com>
2022-07-28 21:28:00 -0500

    input task name

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-timer',
@@ -11,19 +16,35 @@ export class TimerComponent implements OnInit {
     series: 4,
   };
 
+  form: FormGroup = new FormGroup({
+    task: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(5),
+    ]),
+  });
+
   time: number = this.settings.duration;
   seriesCounter: number = 0;
   interval: any;
+  task: string = '';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getTask();
+  }
+
+  getTask() {
+    this.form.get('task')?.valueChanges.subscribe((task) => (this.task = task));
+  }
 
   startTimer() {
     this.interval = setInterval(() => {
       this.time -= 1000;
       this.time === 1497000 && this.completeSerie();
-      this.seriesCounter === this.settings.series && alert('Pomodoro terminado')
+      this.seriesCounter === this.settings.series &&
+        alert('Pomodoro terminado');
     }, 1000);
   }
 
