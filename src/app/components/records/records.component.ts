e52d6afd42e52c9d0ed2bce9e18a48bea4d669ef
Santiago Barrera <santiago.barrerae@gmail.com>
2022-08-03 11:58:15 -0500

    navbar styles

--- src/app/components/records/records.component.ts
+++ src/app/components/records/records.component.ts
@@ -5,24 +5,21 @@ import { IRecord } from 'src/app/interfaces/Irecord.interface';
 @Component({
   selector: 'app-records',
   templateUrl: './records.component.html',
-  styleUrls: ['./records.component.scss']
+  styleUrls: ['./records.component.scss'],
 })
 export class RecordsComponent implements OnInit {
   records: IRecord[] = [];
 
   constructor(public recordService: RecordService) {
     this.records = this.recordService.loadRecords();
-    console.log(this.records)
-   }
-
-  ngOnInit(): void {
+    console.log(this.records);
   }
 
+  ngOnInit(): void {}
 
-  clean(){
-    localStorage.removeItem("userRecords");
+  clean() {
+    localStorage.removeItem('userRecords');
     this.recordService.loadRecords();
-  this.records = [];
-
+    this.records = [];
   }
 }
