6106bb6d0c3ea7d7097a9f576ac47aa7c7050b52
Santiago Barrera <santiago.barrerae@gmail.com>
2022-08-03 00:55:15 -0500

    records route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RecordsComponent } from './components/records/records.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SettingsComponent } from './components/settings/settings.component';
@@ -5,6 +6,7 @@ import { TimerComponent } from './components/timer/timer.component';
 
 const routes: Routes = [
   { path: '', component: TimerComponent },
+  { path: 'records', component: RecordsComponent },
   { path: 'settings', component: SettingsComponent },
 ];
 
